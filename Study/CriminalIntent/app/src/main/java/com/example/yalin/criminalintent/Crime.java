691b8ee2614c3463433c78695317b757a8607d61
Yaoyao Lin <yalin@blackberry.com>
2015-10-06 14:25:46 -0400

    20151006 read to page 167

--- Study/CriminalIntent/app/src/main/java/com/example/yalin/criminalintent/Crime.java
+++ Study/CriminalIntent/app/src/main/java/com/example/yalin/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package com.example.yalin.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -9,9 +10,12 @@ public class Crime {
 
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public Crime() {
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
     public UUID getId() {
@@ -24,5 +28,21 @@ public class Crime {
 
     public void setTitle(String mTitle) {
         this.mTitle = mTitle;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date mDate) {
+        this.mDate = mDate;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        this.mSolved = solved;
     }
 }
--- Study/CriminalIntent/app/src/main/java/com/example/yalin/criminalintent/CrimeActivity.java
+++ Study/CriminalIntent/app/src/main/java/com/example/yalin/criminalintent/CrimeActivity.java
@@ -1,5 +1,7 @@
 package com.example.yalin.criminalintent;
 
+import android.app.FragmentManager;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,6 +14,15 @@ public class CrimeActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction().add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
     }
 
 
