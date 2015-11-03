f5ff8eb002a837ba2fa7ec814207a79f94ae0668
Yaoyao Lin <yalin@blackberry.com>
2015-11-03 17:30:17 -0500

    20151103 to page 394, skip chapter 24 and 25

---
+++ Study/PhotoGalleryActivity/app/src/main/java/com/example/yalin/photogalleryactivity/PhotoGalleryActivity.java
@@ -0,0 +1,15 @@
+package com.example.yalin.photogalleryactivity;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class PhotoGalleryActivity extends SingleFragmentActivity{
+
+    @Override
+    protected Fragment createFragment() {
+        return new PhotoGalleryFragment();
+    }
+}
---
+++ Study/PhotoGalleryActivity/app/src/main/java/com/example/yalin/photogalleryactivity/PhotoGalleryFragment.java
@@ -0,0 +1,33 @@
+package com.example.yalin.photogalleryactivity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.GridView;
+
+/**
+ * Created by yalin on 11/3/2015.
+ */
+public class PhotoGalleryFragment extends Fragment {
+    GridView mGridView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setRetainInstance(true);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+
+        mGridView = (GridView)v.findViewById(R.id.gridView);
+        return v;
+    }
+}
