36f4989fcbb54f06a30f93d4ee3e72fbc7b4bcb9
Yaoyao Lin <yalin@blackberry.com>
2015-11-04 15:55:26 -0500

    20151104 page 423

--- Study/PhotoGalleryActivity/app/src/main/java/com/example/yalin/photogalleryactivity/FlickrFetchr.java
+++ Study/PhotoGalleryActivity/app/src/main/java/com/example/yalin/photogalleryactivity/FlickrFetchr.java
@@ -1,15 +1,33 @@
 package com.example.yalin.photogalleryactivity;
 
+import android.net.Uri;
+import android.util.Log;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 /**
  * Created by yalin on 11/3/2015.
  */
 public class FlickrFetchr {
+    public static final String TAG = "FlickrFetchr";
+    public static final String ENDPOINT = "https://api.flickr.com/services/rest/";
+    public static final String API_KEY = "f52aa08dbba713e9d1b20aaaa2ddb47f";
+    public static final String METHOD_GET_RECENT = "flickr.photos.getRecent";
+    public static final String PARAM_EXTRAS = "extras";
+    public static final String EXTRA_SMALL_URL = "url_s";
+
+    private static final String XML_PHOTO = "photo";
+
     byte[] getUrlBytes(String urlSpec) throws IOException {
         URL url = new URL(urlSpec);
         HttpURLConnection connection = (HttpURLConnection)url.openConnection();
@@ -36,5 +54,49 @@ public class FlickrFetchr {
 
     public String getUrl(String urlSpec) throws IOException {
         return new String(getUrlBytes(urlSpec));
+    }
+
+    public ArrayList<GalleryItem> fetchItems() {
+        ArrayList<GalleryItem> items = new ArrayList<GalleryItem>();
+
+        try {
+            String url = Uri.parse(ENDPOINT).buildUpon()
+                    .appendQueryParameter("method",METHOD_GET_RECENT)
+                    .appendQueryParameter("api_key",API_KEY)
+                    .appendQueryParameter(PARAM_EXTRAS, EXTRA_SMALL_URL)
+                    .build().toString();
+            String xmlString = getUrl(url);
+            Log.i(TAG, "Received xml: " + xmlString);
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser parser = factory.newPullParser();
+            parser.setInput(new StringReader(xmlString));
+
+            parseItems(items, parser);
+        } catch (IOException ioe) {
+            Log.e(TAG, "Failed to fetch items", ioe);
+        } catch (XmlPullParserException xppe) {
+            Log.e(TAG, "Failed to parse items", xppe);
+        }
+        return items;
+    }
+
+    void parseItems(ArrayList<GalleryItem> items, XmlPullParser parser)
+                throws XmlPullParserException, IOException {
+        int eventType = parser.next();
+
+        while (eventType != XmlPullParser.END_DOCUMENT) {
+            if (eventType != XmlPullParser.START_TAG && XML_PHOTO.equals(parser.getName())) {
+                String id = parser.getAttributeValue(null, "id");
+                String caption = parser.getAttributeValue(null, "title");
+                String smallUrl = parser.getAttributeValue(null, EXTRA_SMALL_URL);
+
+                GalleryItem item = new GalleryItem();
+                item.setId(id);
+                item.setCaption(caption);
+                item.setUrl(smallUrl);
+                items.add(item);
+            }
+            eventType = parser.next();
+        }
     }
 }
